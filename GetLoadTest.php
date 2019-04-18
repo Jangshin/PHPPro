75a46ba8836c421c83b2eb01bbec84e52f0b115c
Jangshin <1027077460@qq.com>
2019-04-18 23:35:34 +0800

    添加了管理员的一些功能实现，比如文件上传等

--- GetLoadTest.php
+++ GetLoadTest.php
@@ -1,6 +1,5 @@
 <?php
-if( $_POST["submit"] == "提交"){   // 判断提交的按钮名称是否为“登录”
-    // 使用 echo 语句输出使用 $_POST[] 方法获取的用户名和密码
-    echo "所属类为:". $_POST['lei1'] . "<br >化石种类:" . $_POST['lei2'] . "<br>化石名称：".$_POST['lei3'];
+if( $_POST["submit"] == "提交"){   // 判断提交的按钮名称是否为“提交”
+    echo "所属类为:". $_POST['FileName'] ;
 }
 ?>
--- TestShow.php
+++ TestShow.php
@@ -1,28 +1,31 @@
 <?php
+header("content-Type: text/html;charset=utf-8");
+$DirStr = null;
+$FileName = null;
 // 假定数据库用户名：root，密码：123456，数据库：RUNOOB
 $con=mysqli_connect("localhost","root","123456","mytestdb");
 if (mysqli_connect_errno($con))
 {
-    echo "连接 MySQL 失败: " . mysqli_connect_error();
+    echo "连接 MySQL 失败" . mysqli_connect_error() ."刷新网页试试";
 }
 else {
 // 执行查询
-    echo "连接 MySQL 成功 ";
-    mysqli_query($con, "INSERT INTO test  VALUES ('test')");
+    //mysqli_query($con, "INSERT INTO testtable  VALUES ('test','new')");
     echo "<br>";
-    $sql = "select* from test";
+    $sql = "select* from TestTable";
 //查询
-    $results = $con -> query($sql);
+    mysqli_query($con,'set names utf8');
+    $arr = mysqli_query ( $con, $sql );
 
 //遍历循环打印数据
-    while ($row = mysqli_fetch_array($results))
+    while ($row = mysqli_fetch_array($arr))
     {
-//    print_r($row);
         echo $row['Dir'];
-        echo "<br>";
+        echo $row['FileName'];
     }
+
 //释放
-    mysqli_free_result($results);
+    mysqli_free_result($arr);
 //关闭连接
     mysqli_close($con);
 }
